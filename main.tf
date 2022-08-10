01098971794b12da6063177311e757179d4926bc
ccie7599 <bapley@akamai.com>
2022-08-10 09:55:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,23 +27,9 @@ resource "linode_lke_cluster" "foobar" {
         }
     }
 }
-resource "linode_lke_cluster" "foobar2" {
-    k8s_version = var.k8s_version
-    label = var.label
-    region = "us-east-1"
-    tags = var.tags
-
-    dynamic "pool" {
-        for_each = var.pools
-        content {
-            type  = pool.value["type"]
-            count = pool.value["count"]
-        }
-    }
-}
 //Export this cluster's attributes
 output "kubeconfig" {
-   value = linode_lke_cluster.foobar2.kubeconfig
+   value = linode_lke_cluster.foobar.kubeconfig
    sensitive = true
 }
 
